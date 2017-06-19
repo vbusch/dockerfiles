c3279ae427da9b60a744298b590ab3fd267252a2
Ulf Lilleengen <lulf@redhat.com>
2017-06-19 14:24:55 +0200

    Remove scripts folder

--- scripts/console/prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cd /opt/app-root/src
-npm install
--- scripts/topic-fowarder/added/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-. /usr/local/dynamic-resources/dynamic_resources.sh
-MAX_HEAP=`get_heap_size`
-if [ -n "$MAX_HEAP" ]; then
-	export JAVA_OPTS="-Xms${MAX_HEAP}m -Xmx${MAX_HEAP}m $JAVA_OPTS"
-fi
-exec /opt/topic-forwarder/bin/topic-forwarder
